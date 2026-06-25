1a16197b536baf0a0ea0c15746f030ad9abf5751
Perry Lim <pl91@st-andrews.ac.uk>
2026-06-25 12:39:00 +0100

    Packaged function as a module: can set colorpalette and process Process choices: Show, getdata TODO:getdata

--- Landframegrabber.py
+++ Landframegrabber.py
@@ -1,10 +1,10 @@
 import clr
 import os
-import sys
 import threading
 import cv2
 import numpy as np
 import os
+from enum import Enum
 # Finds the directory of the LandImagerSDK.dll, which should be in the same directory as the script
 cwd = os.getcwd()
 assemblyLocation = cwd+"\\LandImagerSDK.dll"
@@ -12,41 +12,59 @@ print(assemblyLocation)
 clr.AddReference(assemblyLocation)
 clr.AddReference("System")
 
+# python.net Imports from clr
+# Python.net is a dependency!
 import LandImagerSDK as li
-import System.Net.IPAddress as IPAddress
+import System
 
+
+#ENUMS
+class Palette(Enum):
+    Palette1 = 1
+    Palette2 = 2
+    Palette3 = 3
+    Palette4 = 4
+    Palette5 = 5
+    
+class Process(Enum):
+    Show = 1
+    GetData = 2
+
+    
+# Class that handles connection to the Ametek Camera
 class ConnectLANDDialogue:
      
     def __init__(self):
-        # Stores the ConnectionInfo of the connected device
-        self._connectDevice = li.Model.ConnectionInfo(IPAddress.Parse("127.0.0.1"), IPAddress.Parse("255.255.0.0"), 1040, li.Enums.IPMode.Static, "12345", li.Enums.ImagerModel.ARC)
         # List of Devices found
         self._discoveredDevices = []
-        # DeviceAPI for selected device
+        # DeviceAPI for selected device to be returned upon successful connection
         self._connectedDevice = None
 
-    
+    # Finds connectable devices and appends them to self._discoveredDevices
     def discoverDevices(self):
         devices = li.Discovery.DiscoverDevices()
-
         self._discoveredDevices.clear()
         for device in devices:
             self._discoveredDevices.append(self.ConnectionItem(device.ConnectionInfo))
     
-    def connectDevice(self):
-        choice = 0
-        
-        if len(sys.argv) == 1:
-            choice = 0
-        elif isinstance(sys.argv[1], int) and self._discoveredDevices.count >= sys.argv[1]:
-            choice = sys.argv[1]-1
-        else:
+    # Connects to a device of choice parsed in as input integer arg, choosing the arg-th device in self._discoveredDevices, counting from 0
+    # TODO: Implement an Exception on invalid arg
+    def connectDevice(self, arg):
+        if not isinstance(arg, str):
             print("Error: Invalid argument")
         
-        connectDevice = self._discoveredDevices[choice].getConnectionInfo()
-        # There is an additional check in the example code that the ip addresses are valid. This is skipped here as the ip address is not user input.
-        if connectDevice is not None:
-            self._connectedDevice = li.Discovery.DirectConnect(connectDevice)
+        # search for device with matching IPAddress
+        # Assume no two devices have the same IPAddress
+        connection = None
+        
+        for device in self._discoveredDevices:
+            print(device.toString())
+            if device.toString() == arg:
+                connection = device.getConnectionInfo()
+        if connection is not None:
+            self._connectedDevice = li.Discovery.DirectConnect(connection)
+        else:
+            print("No device with IP: "+ arg +" found.")
                 
     class ConnectionItem:
         def __init__(self, connectionInfo):
@@ -57,7 +75,7 @@ class ConnectLANDDialogue:
             return self._connectionInfo
 
         def toString(self):
-            self._connectionInfo.IPAddress.ToString()
+            return self._connectionInfo.IPAddress.ToString()
  
 class FrameGrabber:
     def __init__(self, DeviceAPI):
@@ -66,11 +84,29 @@ class FrameGrabber:
         self._currentFrame = None
         # Threading lock to sync the frame grabbing and image processing threads
         self._frame_event_lock    = threading.Lock()
+        # Variable to store the ThermalFrame for processings
         self._frame_event         = None
         self._frame_event_updated = False
-        # Writable Bitmap format the original uses
-        self._bmp = None
         
+    def setColorPalette(self, choice):
+        temp = None
+        match choice:
+            case Palette.Palette1:
+                temp = li.Enums.Palette.Palette1
+            case Palette.Palette2:
+                temp = li.Enums.Palette.Palette2
+            case Palette.Palette3:
+                temp = li.Enums.Palette.Palette3
+            case Palette.Palette4:
+                temp = li.Enums.Palette.Palette4
+            case Palette.Palette5:
+                temp = li.Enums.Palette.Palette5
+            case _:
+                # Invalid Palette
+                return
+        self._connectedDevice.ColorPalette.SelectedPalette = temp
+        # return self._connectedDevice.ColorPalette.Palette
+
     def connect(self):
         if self._connectedDevice is not None:
             self._connectedDevice.ThermalFrameAvailable += self.onFrame
@@ -90,18 +126,14 @@ class FrameGrabber:
         with self._frame_event_lock:
             # Store a copy of the event to be processed in the main thread. The provided event object is reused
             # by the SDK once the callback returns.
-            print("onFrame using lock!")
             self._frame_event         = args.ThermalFrame.Clone()
             self._frame_event_updated = True
     
-    # Main Thread set up to receive frames
-    def runMain(self):
+    # Main Thread set up to receive frames, arg process takes in an integer to choose how frame is processed
+    def processFrame(self, process):
         self.connect()
+        # This starts a background thread to send the ThermalFrames from the camera
         self.startStreaming()
-        # # Create and start the image grabbing/processing thread
-        # self._thread = threading.Thread(target=self.runListener)
-        # self._thread.start()
-        
         frame_event = None
         
         while(True):
@@ -113,18 +145,21 @@ class FrameGrabber:
 
                     frame_event = self._frame_event
                     self._frame_event_updated = False
-                    print("Frame received on Main")
-                #B8G8R8A8
-                currentFrame = self._frame_event.GetTemperatureBitmap()
-                python_bytes = bytes(currentFrame.PixelData)
-                numpy_bytes = np.frombuffer(python_bytes, dtype=np.uint8)
-                newarr = numpy_bytes.reshape(frame_event.FrameHeight, frame_event.FrameWidth, 4)
-                cv2.imshow('Grabbed', newarr)
-        
-                # Check for keyboard inputs indicating that the user wants to quit by pressing the q key
-                key = cv2.waitKey(1) & 0xFF
-                if key == ord('q'):
-                    break
+                match process:
+                    case Process.Show:
+                        # B8G8R8A8 Format
+                        # Converts the C# bytes to a numpy array
+                        currentFrame = self._frame_event.GetTemperatureBitmap()
+                        python_bytes = bytes(currentFrame.PixelData)
+                        numpy_bytes = np.frombuffer(python_bytes, dtype=np.uint8)
+                        newarr = numpy_bytes.reshape(frame_event.FrameHeight, frame_event.FrameWidth, 4)
+                        cv2.imshow('Grabbed', newarr)
+                        # Check for keyboard inputs indicating that the user wants to quit by pressing the q key
+                        key = cv2.waitKey(1) & 0xFF
+                        if key == ord('q'):
+                            break
+                    case Process.GetData:
+                        continue
 
         
         
@@ -132,10 +167,15 @@ class FrameGrabber:
                 break
         # Clean up
         cv2.destroyAllWindows()
+        # This ends the background thread
         self.stopStreaming()
         self.disconnect()
 
-def main():
+# Arguments: 
+    # IPAddress : String
+    # Palette : Enum
+    # Process :Enum
+def grabFrame(IPAddress, palette, process):
     """
     Main entry point.
     """
@@ -147,16 +187,17 @@ def main():
     if len(connection._discoveredDevices) == 0:
         print("No devices found")
         return
-    connection.connectDevice()
+    connection.connectDevice(IPAddress)
     if connection._connectedDevice is not None:
         print("Connection Success")
         
     # Grab Frames
     frameGrabber = FrameGrabber(connection._connectedDevice)
-    frameGrabber.runMain()
+    frameGrabber.setColorPalette(palette)
+    frameGrabber.processFrame(0)
     return
     
     
 
 if __name__ == "__main__":
-    main()
+    grabFrame("10.1.10.102", Palette.Palette1, Process.Show)
---
+++ caller.py
@@ -0,0 +1,3 @@
+import Landframegrabber
+
+Landframegrabber.grabFrame("10.1.10.102", Landframegrabber.Palette.Palette1, Landframegrabber.Process.Show)
